549995f2adf8608057d7b70ac86ed45c7795a0d3
CsCppKotlin <CsCppKotlin@gmail.com>
2026-02-04 11:03:20 +0300

    Bubble sort  - visual Form

--- BubbleSort/Form1.cs
+++ BubbleSort/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Algorithm;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,46 @@ namespace BubbleSort
 {
     public partial class Form1 : Form
     {
+        AlgorithmBase<int> algorithm = new BubbleSort<int>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int value))
+            { 
+                algorithm.Items.Add(value);
+                label1.Text += value + " ";
+                textBox1.Text = "";
+            }
+                    
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            algorithm.Sort();
+            label1.Text = "";
+            foreach (var item in algorithm.Items)
+            {
+                label1.Text += item + " ";
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var rnd = new Random();
+            var count = rnd.Next(4, 200);
+            for (int i = 0; i < count; i++)
+            {
+                algorithm.Items.Add(rnd.Next(0, 99));
+            }
+            foreach (var item in algorithm.Items)
+            {
+                label1.Text += item + " ";
+            }
         }
     }
 }
